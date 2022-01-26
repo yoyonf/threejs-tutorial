5ea46048dd3d5003f47e332c1f534dd1fd7d9971
akinsola <thomas.lawanson@gmail.com>
2022-01-26 13:24:49 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Home from './Components/Pages/Home';
-import Test from './Components/Pages/Test';
 import Portfolio from './Components/Pages/Portfolio';
+import Cube from './Components/Pages/Cube';
 
 function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route exact path="/" element={<Home />} />
-				<Route exact path="/cube" element={<Test />} />
+				<Route exact path="/cube" element={<Cube />} />
 				<Route exact path="/portfolio" element={<Portfolio />} />
 			</Routes>
 		</Router>
