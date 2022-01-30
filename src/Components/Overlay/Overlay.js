1b8df4bdcdb182a32402cc734df240cd83b21d56
akinsola <thomas.lawanson@gmail.com>
2022-01-30 10:30:56 +0000

    Update Overlay.js

--- src/Components/Overlay/Overlay.js
+++ src/Components/Overlay/Overlay.js
@@ -1,25 +1,55 @@
 import React, { useState, useEffect, useRef, Component } from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const OverlayWrapper = styled.div`
-    position: absolute;
-    width: 100vw;
-    height: 100vh;
-    background: rgba(255,0,0,10);
-    display: ${props => props.show ? 'block': 'none'}
+	position: absolute;
+	width: 100vw;
+	height: 100vh;
+	background: rgba(0, 255, 0);
+	display: ${(props) => (props.show ? 'block' : 'none')};
+`;
+
+const FlexWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	width: 100vw;
+	height: 100vh;
+`;
+
+const VideoIFrame = styled.iframe`
+	width: 70vw;
+	height: 70vh;
+`;
+
+const Title = styled.h1`color: blue;`;
+
+const CloseOverlay = styled.p`
+    color: blue;
 `
+const Overlay = (props) => {
+	const onClick = () => {
+		props.hide();
+	};
 
-const Overlay = props => {
-
-    const onClick  = () => {
-        props.hide();
-    }
-    return (
-        <OverlayWrapper show={props.show} onClick={() => onClick()}>
-            <p>Overlay </p> 
-        </OverlayWrapper>
-    )
-
-}
+	console.log('OVERLAY', props.project);
+	return (
+		<OverlayWrapper show={props.show} >
+			<FlexWrapper>
+				{props.project ? <Title>{props.project.title} </Title> : null}
+				{props.project && props.project.type == 'VIDEO' && props.project.video_url ? (
+					<VideoIFrame
+						src={props.project.video_url}
+						title={props.project.title}
+						frameBorder="0"
+						allowFullScreen={true}
+					/>
+				) : null}
+                <CloseOverlay onClick={() => onClick()}> close </CloseOverlay>
+			</FlexWrapper>
+		</OverlayWrapper>
+	);
+};
 
 export default Overlay;
