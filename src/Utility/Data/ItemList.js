1a6e9ab03ae4065044e70ba116864376ac9fcfc2
akinsola <thomas.lawanson@gmail.com>
2022-01-30 10:31:03 +0000

    Create ItemList.js

---
+++ src/Utility/Data/ItemList.js
@@ -0,0 +1,8 @@
+import { OverlayItem } from "../Models/OverlayItem";
+
+export const ITEM_LIST = {
+    ITEM_ONE: new OverlayItem("Soy Cuba", "VIDEO", "https://www.youtube.com/embed/BwEabZrGFfI", null), // Film
+	ITEM_TWO: new OverlayItem("The Pharcyde - Drop", "VIDEO", "https://www.youtube.com/embed/wqVsfGQ_1SU", null), // Music Video
+	ITEM_THREE: new OverlayItem("", "IMAGE", null, null) // 
+}
+
