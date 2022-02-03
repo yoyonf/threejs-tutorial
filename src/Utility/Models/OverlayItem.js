9c6a689ef4a7852437e532f462270f0116611113
akinsola <thomas.lawanson@gmail.com>
2022-02-03 22:03:49 +0000

    Update OverlayItem.js

--- src/Utility/Models/OverlayItem.js
+++ src/Utility/Models/OverlayItem.js
@@ -2,12 +2,10 @@ export class OverlayItem {
     title;
     type;
     video_url;
-    image_url;
 
-    constructor(title, type, video_url, image_url) {
+    constructor(title, type, video_url) {
         this.title = title;
         this.type = type;
         this.video_url = video_url;
-        this.image_url = image_url;
     }
 }
