50c5a631c356afd39789635d9972a4c3e0ddae2f
akinsola <thomas.lawanson@gmail.com>
2022-02-03 22:03:47 +0000

    Update ItemList.js

--- src/Utility/Data/ItemList.js
+++ src/Utility/Data/ItemList.js
@@ -1,8 +1,8 @@
 import { OverlayItem } from "../Models/OverlayItem";
 
 export const ITEM_LIST = {
-    SOY_CUBA: new OverlayItem("Soy Cuba", "VIDEO", "https://www.youtube.com/embed/BwEabZrGFfI", null), // Film
-	PHARCYDE: new OverlayItem("The Pharcyde - Drop", "VIDEO", "https://www.youtube.com/embed/wqVsfGQ_1SU", null), // Music Video
-	MONA_LISA: new OverlayItem("Mona Lisa", "IMAGE", null, null) // 
+    SOY_CUBA: new OverlayItem("Soy Cuba", "VIDEO", "https://www.youtube.com/embed/BwEabZrGFfI"), // Film
+	PHARCYDE: new OverlayItem("The Pharcyde - Drop", "VIDEO", "https://www.youtube.com/embed/wqVsfGQ_1SU"), // Music Video
+	MONA_LISA: new OverlayItem("Mona Lisa", "IMAGE", null) // 
 }
 
